a6487e8061680eae860ea32785a51d212ee12653
WeaverD9028 <dcw025@latech.edu>
2020-05-16 15:15:25 -0500

    New User GUI

--- New User GUI.py
+++ New User GUI.py
@@ -36,9 +36,27 @@ class ThirdGUI(Frame):
         button.image = img
         button.grid(row = 4, column = 0, sticky = N+E+W+S)
 
+        ##################Submit Button class########################
 
+        
+        class App(Frame):
+            def __init__(self, master):
+                Frame.__init__(self,master)
+                self.label = Label(master, text = "Enter email")
+                self.label.pack(side = LEFT)
+                self.entry = Entry(master)
+                self.entry.pack(side = LEFT)
+                self.button1 = Button(master, text = "Submit",command = self.Write)
+                self.button1.pack(side = LEFT)
+
+            def Write(self):
+                print "place tag"
+                #cards.append(Id)
+                
 ##################Window##################
 window = Tk()
+
+app = App(window)
 
 window.title("Keypad")
 
